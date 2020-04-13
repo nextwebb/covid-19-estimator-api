c097790fc27f7871b4e4e381b3d87a2bd26c15c9
nextwebb <oaikhenahpeterson@gmail.com>
2020-04-14 00:22:36 +0100

    updated packages

--- db.js
+++ db.js
@@ -3,15 +3,15 @@ dotenv.config()
 const mongodb = require('mongodb')
 
 // eslint-disable-next-line max-len
-mongodb.connect(process.env.connectionstring, { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
+mongodb.connect(process.env.CONNECTIONSTRING, { useUnifiedTopology: true, useNewUrlParser: true }, (err, client) => {
     if (err){
         console.log('database could not connect!')
     } else{
         // This anonymous function auto runs and mutates the variable "db", upon connection
-  module.exports = client;
+  module.exports = client
   const app = require('./app')
-  app.listen(process.env.port);// only upon connection should you listen to this port
-  console.log(`Listening on port ${process.env.port}`);
+  app.listen(process.env.PORT);// only upon connection should you listen to this port
+  console.log(`Listening on port ${process.env.PORT}`);
     } 
   
 });
